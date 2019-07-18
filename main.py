43bbefbd626cff6f4086995c2825371cc7f58f0d
GeraldBaratoux <geraldbaratoux@gmail.com>
2019-07-18 11:47:50 +0200

    ajout lecture fichier

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import sys
 from PySide2.QtWidgets import QApplication, QMainWindow
 from ui_vlc import Ui_MainWindow
+from PySide2.QtCore import QUrl
+from PySide2.QtMultimedia import QMediaPlayer, QMediaContent
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.pbLecture.clicked.connect(self.lectureClicked)
 
+        self.mediaPlayer = QMediaPlayer()
+        self.mediaPlayer.setVideoOutput(self.ui.wVideo)
+
+    def lectureClicked(self):
+        print("Lecture !!")
+        mediaContent = QMediaContent(QUrl.fromLocalFile("big_buck_bunny.avi"))
+        self.mediaPlayer.setMedia(mediaContent)
+        self.mediaPlayer.play()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
