79d3f5dcc131c61f2c1ab18fc2c7ac3aee3420f9
GeraldBaratoux <geraldbaratoux@gmail.com>
2019-07-19 10:26:01 +0200

    Gestion Playlist 1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog, QListWidgetItem
 from ui_vlc import Ui_MainWindow
-from PySide2.QtCore import QUrl, QTime
+from PySide2.QtCore import QUrl, QTime, QFileInfo
 from PySide2.QtMultimedia import QMediaPlayer, QMediaContent
 
 class MainWindow(QMainWindow):
@@ -13,15 +13,19 @@ class MainWindow(QMainWindow):
         self.ui.pbPause.clicked.connect(self.pauseClicked)
         self.ui.pbStop.clicked.connect(self.stopClicked)
         self.ui.dVolume.valueChanged.connect(self.volumeChanged)
+        self.ui.sTpsCourant.valueChanged.connect(self.sliderPositionChanged)
+        self.ui.pbAjout.clicked.connect(self.ajouterMedia2)
+        self.ui.pbSuppr.clicked.connect(self.supprMedia)
+        self.ui.lPlaylist.itemDoubleClicked.connect(self.mediaSelected2)
 
         self.mediaPlayer = QMediaPlayer()
         self.mediaPlayer.setVideoOutput(self.ui.wVideo)
 
         self.mediaPlayer.durationChanged.connect(self.mediaDurationChanged)
-        self.mediaPlayer.positionChanged.connect(self.mediapositionChanged)
+        self.mediaPlayer.positionChanged.connect(self.mediaPositionChanged)
 
-        mediaContent = QMediaContent(QUrl.fromLocalFile("big_buck_bunny.avi"))
-        self.mediaPlayer.setMedia(mediaContent)
+        #mediaContent = QMediaContent(QUrl.fromLocalFile("big_buck_bunny.avi"))
+        #self.mediaPlayer.setMedia(mediaContent)
 
     def lectureClicked(self):
         print("Lecture !!")
@@ -37,7 +41,6 @@ class MainWindow(QMainWindow):
     def volumeChanged(self):
         self.mediaPlayer.setVolume(self.ui.dVolume.value())
         self.ui.lVolume.setText(str(self.ui.dVolume.value())+"%")
-
     def mediaDurationChanged(self):
         print("mediaLoaded")
         self.ui.lTpsCourant.setText("00:00:00")
@@ -46,14 +49,50 @@ class MainWindow(QMainWindow):
         totalTimeMedia = QTime(0, 0, 0)
         totalTimeMedia = totalTimeMedia.addMSecs(mediaDuration)
         self.ui.lTpsTotal.setText(totalTimeMedia.toString("HH:mm:ss"))
-
-    def mediapositionChanged(self):
+    def mediaPositionChanged(self):
+        self.ui.sTpsCourant.valueChanged.disconnect(self.sliderPositionChanged)
         mediaPosition = self.mediaPlayer.position()
         self.ui.sTpsCourant.setValue(mediaPosition)##############
         currentTimeMedia = QTime(0, 0, 0)
         currentTimeMedia = currentTimeMedia.addMSecs(mediaPosition)
         self.ui.lTpsCourant.setText(currentTimeMedia.toString("HH:mm:ss"))
+        self.ui.sTpsCourant.valueChanged.connect(self.sliderPositionChanged)
+    def sliderPositionChanged(self):
+        self.mediaPlayer.positionChanged.disconnect(self.mediaPositionChanged)
+        self.mediaPlayer.setPosition(self.ui.sTpsCourant.value())
+        self.mediaPlayer.positionChanged.connect(self.mediaPositionChanged)
 
+    def ajouterMedia(self):
+        nomMedia = QFileDialog.getOpenFileName(self,"Choix Film", "c:/", "Movie Files (*.avi *.mp4)")
+        item = QListWidgetItem(nomMedia[0])
+        self.ui.lPlaylist.addItem(item)
+
+    def ajouterMedia2(self):
+        nomMedia = QFileDialog.getOpenFileName(self, "Choix Film", "c:/", "Movie Files (*.avi *.mp4)")
+        fInfo = QFileInfo(nomMedia[0])
+        fShortName = fInfo.baseName()
+        item = QListWidgetItem(fShortName)
+        item.setToolTip(nomMedia[0])
+        self.ui.lPlaylist.addItem(item)
+
+    def supprMedia(self):
+        rowItem = self.ui.lPlaylist.currentRow()
+        if rowItem != -1:
+            self.ui.lPlaylist.takeItem(rowItem)
+
+    #self.ui.lPlaylist.itemDoubleClicked.connect(self.mediaSelected)
+
+    def mediaSelected(self):
+        currentItem = self.ui.lPlaylist.currentItem()
+        mediaContent = QMediaContent(QUrl.fromLocalFile(currentItem.text()))
+        self.mediaPlayer.setMedia(mediaContent)
+        self.lectureClicked()
+
+    def mediaSelected2(self):
+        currentItem = self.ui.lPlaylist.currentItem()
+        mediaContent = QMediaContent(QUrl.fromLocalFile(currentItem.toolTip()))
+        self.mediaPlayer.setMedia(mediaContent)
+        self.lectureClicked()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
